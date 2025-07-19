c2290a3f3ba8d89f72131ff4f83f21b5d0b15378
Juaner_ <juanrape2000@gmail.com>
2025-07-19 15:46:59 -0500

    Initial commit

---
+++ Source/ZelkarPruebaTecnica/ZelkarPruebaTecnicaGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ZelkarPruebaTecnicaGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZELKARPRUEBATECNICA_API AZelkarPruebaTecnicaGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
